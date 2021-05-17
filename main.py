4e1b1a71dbb65796e1ec0fefff568e78c2f7df57
gguilherme42 <58306122+gguilherme42@users.noreply.github.com>
2021-05-17 00:35:51 -0300

    Add level and lives label to the screen.

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ import time
 import random
 
 
+# The font object needs to be initialized
+pygame.font.init()
+
 WIDTH, HEIGHT = 650, 600
 # Pygame surfice
 WINDOW = pygame.display.set_mode((WIDTH, HEIGHT))
@@ -30,12 +33,22 @@ BG =  pygame.transform.scale(pygame.image.load(os.path.join("assets", "backgroun
 def main():
     run = True
     FPS = 60
+    level = 1
+    lives = 5
+    main_font = pygame.font.SysFont("comicsans", 50)
+
     clock = pygame.time.Clock()
 
     
     def redraw_window():
         # Starts  with the BG to fill the others images
         WINDOW.blit(BG, (0, 0))
+        # Draw text 
+        lives_label = main_font.render(f"Lives: {lives}", 1, (255, 255, 255))
+        level_label = main_font.render(f"Level: {level}", 1, (255, 255, 255))
+
+        WINDOW.blit(lives_label, (10, 10))
+        WINDOW.blit(level_label, (WIDTH - level_label.get_width() - 10, 10))
         
         pygame.display.update()
     
