33dc13947b43c98df9d896aaa52b41c7a71df4d2
gguilherme42 <58306122+gguilherme42@users.noreply.github.com>
2021-05-17 00:04:33 -0300

    Add main loop with the basic.

--- main.py
+++ main.py
@@ -26,4 +26,17 @@ YELLOW_LASER = pygame.image.load(os.path.join("assets", "pixel_laser_yellow.png"
 BG =  pygame.image.load(os.path.join("assets", "background-black.png"))
 
 
+def main():
+    run = True
+    FPS = 60
+    clock = pygame.time.Clock()
 
+    while run:
+        clock.tick(FPS)
+
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False  
+
+main()
