094b746456fc83203c8d0a01530571639a7f731b
gguilherme42 <58306122+gguilherme42@users.noreply.github.com>
2021-05-17 13:08:19 -0300

    Add collide function and Laser class.

--- main.py
+++ main.py
@@ -31,6 +31,34 @@ YELLOW_LASER = pygame.image.load(os.path.join("assets", "pixel_laser_yellow.png"
 # Backgorund
 BG =  pygame.transform.scale(pygame.image.load(os.path.join("assets", "background-black.png")), (WIDTH, HEIGHT))
 
+
+class Laser:
+    def __init__(self, x, y, img):
+        self.x = x
+        self.y = y
+        self.img = img
+        self.mask = pygame.mask.from_surface(self.img)
+
+
+    def draw(self, window):
+        window.blit(self.img, (self.x, self.y))
+
+    
+    def move(self, velocity):
+        self.y += velocity
+
+    
+    def off_screen(self, height):
+        return 0 <= self.y <= height 
+
+
+    def collision(self, obj):
+        return collide(obj, self)
+    
+
+
+
+
 class Ship:
     def __init__(self, x_position, y_position, health=100):
         self.x = x_position
@@ -78,7 +106,14 @@ class Enemy(Ship):
     
     def move(self, velocity):
         self.y += velocity
-        
+
+
+
+def collide(obj1, obj2):
+    offset_x = obj2.x - obj1.x
+    offset_y = obj2.y - obj1.y
+    return obj1.mask.overlap(obj2, (offset_x, offset_y)) != None
+
 
 def main():
     run = True
